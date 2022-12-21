9f0cb02614805ad2a0b20e12976fccfb5037d813
hellosooka <leo.zemtsoff@icloud.com>
2022-12-21 15:20:16 +0300

    test vercel

--- src/views/SearchRoomView.vue
+++ src/views/SearchRoomView.vue
@@ -1,31 +1,64 @@
 <script setup>
 import { computed, ref } from "vue";
+import { useAuthStore } from "../stores/auth";
+import axios from "axios";
 
-const name = ref("");
+const nickname = ref("");
 const code = ref("");
+const isError = ref(false);
 
 const isActive = computed(() => {
-  if (name.value.trim() == "" || code.value.trim() == "") {
+  if (nickname.value.trim() == "" || code.value.trim() == "") {
+    isError.value = false;
     return false;
   } else {
     return true;
   }
 });
+
+const authStore = useAuthStore();
+
+const onSubmit = async () => {
+  const room = await getRoomByRoomCode();
+  await addUserToRoom(room.id);
+  //window.location.href = `https://sgb-bunker-client.vercel.app/room/${room.roomCode}`;
+};
+
+const getRoomByRoomCode = async () => {
+  try {
+    const response = await axios.get(
+      `https://sgb-room-service.vercel.app/room/${code.value}/byRoomCode`
+    );
+    isError.value = false;
+    return response.data;
+  } catch (e) {
+    isError.value = true;
+    return false;
+  }
+};
+
+const addUserToRoom = async (roomId) => {
+  try {
+    await axios.post(
+      `https://sgb-room-service.vercel.app/room/add-client/${roomId}`,
+      { login: authStore.nickname, nickname: nickname.value }
+    );
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 </script>
 
 <template>
   <div class="container">
-    <form class="form_container">
-      <label for="name" class="label" v-upper-case> Имя </label>
-      <input v-model="name" id="name" class="input" type="text" />
+    <form @submit.prevent="onSubmit" class="form_container">
+      <span class="error_message" v-if="isError"> Комната не найдена </span>
+
+      <label for="nickname" class="label" v-upper-case> Имя </label>
+      <input v-model="nickname" id="nickname" class="input" type="text" />
       <label for="code" class="label" v-upper-case> Код комнаты </label>
       <input v-model="code" id="code" class="input" type="text" />
-      <button
-        :disabled="!isActive"
-        :class="{ active: isActive }"
-        class="button"
-        type="submit"
-      >
+      <button :disabled="!isActive" :class="{ active: isActive }" class="button" type="submit">
         Играть
       </button>
     </form>
@@ -39,11 +72,28 @@ const isActive = computed(() => {
   background-color: #141414;
   padding-bottom: 50px;
 }
+
 .form_container {
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  transition: 300ms;
 }
+
+.error_message {
+  font-family: "St_Sign normal";
+  font-size: 2vw;
+  text-align: center;
+  padding: 1vw;
+  margin-top: 2vw;
+  background-color: #a83142;
+  color: white;
+  border-radius: 5px;
+
+  transition: 300ms;
+}
+
 .label {
   margin-top: 20px;
   margin-left: 9px;
@@ -76,10 +126,18 @@ const isActive = computed(() => {
   color: white;
   border: none;
 }
+
 @media (max-width: 450px) {
+  .error_message {
+    margin-top: 4vw;
+    padding: 2vw;
+    font-size: 5vw;
+  }
+
   .label {
     font-size: 25px;
   }
+
   .form_container {
     width: 240px;
   }
